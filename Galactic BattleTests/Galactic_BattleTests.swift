83b1f2fa4d09c37eaee91a7e577d1c316646fffa
Matthew Allen Lin <Darthpwner@Matthews-MacBook-Pro-2.local>
2015-09-19 17:01:38 -0700

    Added files

---
+++ Galactic BattleTests/Galactic_BattleTests.swift
@@ -0,0 +1,36 @@
+//
+//  Galactic_BattleTests.swift
+//  Galactic BattleTests
+//
+//  Created by Matthew Allen Lin on 9/19/15.
+//  Copyright © 2015 MAL Software. All rights reserved.
+//
+
+import XCTest
+@testable import Galactic_Battle
+
+class Galactic_BattleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
